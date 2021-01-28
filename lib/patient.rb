f56e9ecdb3c2c440791d8e25e97ccf82554eb401
seireitou <ausis@mail.com>
2021-01-27 22:56:59 -0600

    commit tree 7a792fad9eec870ce5d2945b387889f0da0212d0

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
 
+class Patient
+    attr_accessor :name
+    @@all = []
+    
+    def initialize(name)
+        self.name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        appointment = Appointment.new(date, self, doctor)
+    end
+
+
+    def appointments
+        Appointment.all.select do |song|
+            song.patient == self
+        end
+    end
+
+    def doctors
+        self.appointments.map do |appointment|
+            appointment.doctor
+        end
+    end
+end
